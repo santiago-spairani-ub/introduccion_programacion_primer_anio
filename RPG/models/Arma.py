65d599aa8c3fc77c8c1ef6e153fd5b36d3428c62
Santiago Spairani <santiagospairani@gmail.com>
2026-06-28 23:12:36 -0300

    agrego comentario

--- RPG/models/Arma.py
+++ RPG/models/Arma.py
@@ -6,6 +6,7 @@ class Arma:
         self._danio_minimo = danio_minimo;
         self._danio_maximo = danio_maximo;
 
+    #override del metodo str para mostrar el nombre y el rango de daño del arma
     def __str__(self):
         return f"{self._nombre} ({self._danio_minimo}-{self._danio_maximo})";
 
